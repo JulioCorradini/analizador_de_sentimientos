2c2d5e1faecb7bf11f227923ac6f87da9e154fb2
Julio Corradini <julcorradi@gmail.com>
2023-11-21 20:41:22 -0300

    Cambiando el CSS.

--- Datos_de_entrenamiento.py
+++ Datos_de_entrenamiento.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
 #Ruta del archivo CSV
-ruta_arcivo_CSV = 'C:/Users/Nadia/Desktop/Proyectos_de_programacion/Programas hechos con Visual Studio/Analizador_de_sentimientos/test.csv'
+##ruta_arcivo_CSV = 'C:/Users/Nadia/Desktop/Proyectos_de_programacion/Programas hechos con Visual Studio/Analizador_de_sentimientos/test.csv'
+ruta_archivo_CSV = 'C:/Users/Julio/Desktop/Proyectos_de_programacion/analizador_de_sentimientos/test.csv'
 
 #Leer el archivo CSV con Pandas
-datos = pd.read_csv(ruta_arcivo_CSV, encoding='ISO-8859-1', header=None, skiprows=1)
+datos = pd.read_csv(ruta_archivo_CSV, encoding='ISO-8859-1', header=None, skiprows=1)
 
 #Crear un nuevo Data Frame con las columnas Sentimiento y Tweet
 dataset = datos[[1, 2]]
