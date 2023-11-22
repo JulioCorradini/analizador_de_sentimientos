5cc143fa5bd724f844e046852a3519d4e3489e20
Julio Corradini <julcorradi@gmail.com>
2023-11-22 13:35:39 -0300

    Arreglando el CSS, y modificando la ruta del archivo CSV que se usa como datset.

--- Datos_de_entrenamiento.py
+++ Datos_de_entrenamiento.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
 #Ruta del archivo CSV
-##ruta_arcivo_CSV = 'C:/Users/Nadia/Desktop/Proyectos_de_programacion/Programas hechos con Visual Studio/Analizador_de_sentimientos/test.csv'
-ruta_archivo_CSV = 'C:/Users/Julio/Desktop/Proyectos_de_programacion/analizador_de_sentimientos/test.csv'
+ruta_archivo_CSV = './test.csv'
 
 #Leer el archivo CSV con Pandas
 datos = pd.read_csv(ruta_archivo_CSV, encoding='ISO-8859-1', header=None, skiprows=1)
